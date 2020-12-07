244d5491f822a09073f007d09c8ef29ec504f565
Andreas Buff <andreas@pep-security.net>
2020-12-07 20:08:58 +0100

    FEATURE-1 extremly dirty and buggy, but item is added and click received. I am afraid Mailtoolbar does not depend only on delegate. This somthingPlist() method needs investigation ...

---
+++ bufFPlug/src/UnusedDelete/MessageViewer+BPAdditions.h
@@ -0,0 +1,17 @@
+//
+//  MessageViewer+BPAdditions.h
+//  bufFPlug
+//
+//  Created by Andreas Buff on 07.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "MessageViewer.h"
+
+NS_ASSUME_NONNULL_BEGIN
+@interface MessageViewer(BPAdditions)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ bufFPlug/src/UnusedDelete/MessageViewer+BPAdditions.m
@@ -0,0 +1,12 @@
+//
+//  MessageViewer+BPAdditions.m
+//  bufFPlug
+//
+//  Created by Andreas Buff on 07.12.20.
+//
+
+#import "MessageViewer+BPAdditions.h"
+
+@implementation MessageViewer(BPAdditions)
+
+@end
