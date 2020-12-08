4318d1c86669dbda58ab6a3e1f113317e58f4da0
Andreas Buff <andreas@pep-security.net>
2020-12-08 18:57:25 +0100

    FEATURE-1 re-adds injector classes - does _not_ load

---
+++ bufFPlug/src/MailInjectorExtensions/MailToolbar+BPSwizzle.h
@@ -0,0 +1,17 @@
+//
+//  MailToolbar+BPSwizzle.h
+//  bufFPlug
+//
+//  Created by Andreas Buff on 08.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString* const bpToolbarItemIdentifier;
+
+@interface MailToolbar_BP : NSObject
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ bufFPlug/src/MailInjectorExtensions/MessageViewer+BPSwizzle.m
@@ -0,0 +1,57 @@
+//
+//  MessageViewer+BPSwizzle.m
+//  bufFPlug
+//
+//  Created by Andreas Buff on 08.12.20.
+//
+
+#import "MessageViewer+BPSwizzle.h"
+
+#import <AppKit/AppKit.h>
+#import "MailToolbar+BPSwizzle.h"
+
+
+@implementation MessageViewer_BP
+
+- (NSToolbarItem *)bp_toolbar:(NSToolbar *)arg1 itemForItemIdentifier:(NSString *)arg2 willBeInsertedIntoToolbar:(BOOL)arg3 {
+    if (![arg2 isEqualToString:bpToolbarItemIdentifier]) {
+        return [self bp_toolbar:arg1 itemForItemIdentifier:arg2 willBeInsertedIntoToolbar:arg3];
+    }
+    // Make sure our toolbar item was not already added
+    for(NSToolbarItem *item in [arg1 items]) {
+        if([item.itemIdentifier isEqualToString:bpToolbarItemIdentifier])
+            return nil;
+    }
+
+    NSSize toolbarItemSize = NSMakeSize(75.0, 23.0);
+    NSToolbarItem *item = [[NSToolbarItem alloc] initWithItemIdentifier:bpToolbarItemIdentifier];
+    //    [item setView:securityMethodAccessoryView];
+    item.title = @"Feature 1";
+    item.minSize = toolbarItemSize;
+
+    item.target = self;
+    item.action = @selector(toolbarItemAction:);
+    return item;
+}
+
+- (void)toolbarItemAction:(id *)sender {
+    NSLog(@"Clicked!");
+}
+
+- (NSArray *)bp_toolbarAllowedItemIdentifiers:(NSToolbar *)arg1 {
+    NSMutableArray *allowedIdentifiers = [self bp_toolbarAllowedItemIdentifiers:arg1].mutableCopy;
+    if (![allowedIdentifiers containsObject:bpToolbarItemIdentifier]) {
+        // Only if not in already
+        [allowedIdentifiers addObject:bpToolbarItemIdentifier];
+    }
+    return allowedIdentifiers;
+}
+
+- (void)bp_toolbarWillAddItem:(NSNotification *)arg1 {
+    NSToolbarItem *item = arg1.userInfo[@"item"];
+    if ([item.itemIdentifier isEqualToString:bpToolbarItemIdentifier]) {
+        // We might get a handle on it here to update it later
+    }
+    [self bp_toolbarWillAddItem:arg1];
+}
+@end
