f0a2c6d4bfe61fb2871e92c10a503578ebcaf9cd
Andreas Buff <andreas@pep-security.net>
2020-12-08 18:33:46 +0100

    FEATURE-1 uses new CodeInjector approach. (and cleans up)

---
+++ bufFPlug/src/MailInjectorExtensions/MailToolbar+BPSwizzle.m
@@ -0,0 +1,38 @@
+//
+//  MailToolbar+BPSwizzle.m
+//  bufFPlug
+//
+//  Created by Andreas Buff on 08.12.20.
+//
+
+#import "MailToolbar+BPSwizzle.h"
+#import <AppKit/AppKit.h>
+
+NSString* const bpToolbarItemIdentifier = @"bpToolbarItemIdentifier";
+
+@implementation MailToolbar_BP
+
+- (NSArray *)bp_toolbarDefaultItemIdentifiers:(NSToolbar *)arg1 {
+    NSMutableArray *defaultIdentifiers = [self bp_toolbarDefaultItemIdentifiers:arg1].mutableCopy;
+    if (![defaultIdentifiers containsObject:bpToolbarItemIdentifier]) {
+        // Only if not in already
+        [defaultIdentifiers addObject:bpToolbarItemIdentifier];
+    }
+    return defaultIdentifiers;
+}
+
++ (id)bp__plistForToolbarWithIdentifier:(id)arg1 {
+    id ret = [self bp__plistForToolbarWithIdentifier:arg1];
+
+    if(![arg1 isEqualToString:@"MainWindow"])
+        return ret;
+
+    NSMutableDictionary *configuration = [ret mutableCopy];
+    NSMutableArray *defaultSet = [configuration[@"default set"] mutableCopy];
+    [defaultSet addObject:bpToolbarItemIdentifier   ];
+    [configuration setObject:defaultSet forKey:@"default set"];
+
+    return configuration;
+}
+
+@end
