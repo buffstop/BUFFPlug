ca0dfb902b04e00a7b3f0e04b6f2bd50645df68b
Andreas Buff <andreas@pep-security.net>
2020-12-16 18:40:03 +0100

    FEATURE-2 show headers

--- bufFPlug/MailBundle/BPHeadersViewController.h
+++ bufFPlug/MailBundle/BPHeadersViewController.h
@@ -11,6 +11,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface BPHeadersViewController : NSViewController<NSTableViewDelegate, NSTableViewDataSource>
 
++ (instancetype)newIntance;
+
+@end
+
+// Debugger issues defining cell in independent file
+@interface BPValueCell : NSTableCellView
+@property (weak) IBOutlet NSScrollView *scrollView;
+@property (unsafe_unretained) IBOutlet NSTextView *textView;
+
+-(void)addLinkDetection;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- bufFPlug/MailBundle/MessageViewer+BPSwizzle.h
+++ bufFPlug/MailBundle/MessageViewer+BPSwizzle.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MessageViewer_BP : NSObject
 
-+ (void)updateOurToolbarItemWithHeaders:(MCMessageHeaders*)newHeaders;
++ (void)updatePopoverWithHeaders:(MCMessageHeaders*)newHeaders;
 
 @end
 
